9b0f539e3f23d4150dd38166574a076cf1c82245
Kaushal Satani <kaushal.mbinfoways@gmail.com>
2025-04-10 22:46:23 +0530

    nav controller added; kaushal;

--- composeApp/src/androidMain/kotlin/org/example/create/company/flow/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/create/company/flow/MainActivity.kt
@@ -2,22 +2,23 @@ package org.example.create.company.flow
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(Color.Black.toArgb(), Color.Black.toArgb()),
+            navigationBarStyle = SystemBarStyle.light(scrim = Color.Transparent.toArgb(), darkScrim = Color.Transparent.toArgb())
+        )
         setContent {
             App()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
